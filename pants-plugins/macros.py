15d251f23b3c69282c18cc52b188175af0442efa
sj <suresh@vicarasolutions.com>
2024-06-16 16:57:59 -0400

    Testing GHCR as a cache location

--- pants-plugins/macros.py
+++ pants-plugins/macros.py
@@ -11,3 +11,13 @@ def docker_image_gha(name: str, **kwargs) -> None:
         cache_to=cache_to,
         **kwargs,
     )
+
+    cache_from = kwargs.pop("cache_from", {"type": "registry", "ref": "robotpajamas/dockerfiles-build-cache:my-branch"})
+    cache_to = kwargs.pop("cache_to", {"type": "registry", "mode": "max", "ref": "robotpajamas/dockerfiles-build-cache:my-branch"})
+
+    docker_image(  # noqa: F821
+        name=name,
+        cache_from=cache_from,
+        cache_to=cache_to,
+        **kwargs,
+    )
