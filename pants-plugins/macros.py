70e4487c00ad32fab14604ac3cd49e4886f5d423
sj <suresh@vicarasolutions.com>
2024-06-16 16:59:28 -0400

    Forgot to commit this

--- pants-plugins/macros.py
+++ pants-plugins/macros.py
@@ -16,7 +16,7 @@ def docker_image_gha(name: str, **kwargs) -> None:
     cache_to = kwargs.pop("cache_to", {"type": "registry", "mode": "max", "ref": "robotpajamas/dockerfiles-build-cache:my-branch"})
 
     docker_image(  # noqa: F821
-        name=name,
+        name=f"{name}-ghcr",
         cache_from=cache_from,
         cache_to=cache_to,
         **kwargs,
