32f70280925512f94dff269bfc2e5d3e6712e491
sj <suresh@vicarasolutions.com>
2024-06-16 21:28:49 -0400

    Testing a different cache namespacing

--- pants-plugins/macros.py
+++ pants-plugins/macros.py
@@ -2,21 +2,21 @@ def docker_image_gha(name: str, **kwargs) -> None:
     """
     A macro to create a Docker image that caches to Github Actions, by default.
     """
-    cache_from = kwargs.pop("cache_from", {"type": "gha", "scope": name})
-    cache_to = kwargs.pop("cache_to", {"type": "gha", "mode": "max", "scope": name})
+    # cache_from = kwargs.pop("cache_from", {"type": "gha", "scope": name})
+    # cache_to = kwargs.pop("cache_to", {"type": "gha", "mode": "max", "scope": name})
 
-    docker_image(  # noqa: F821
-        name=name,
-        cache_from=cache_from,
-        cache_to=cache_to,
-        **kwargs,
-    )
+    # docker_image(  # noqa: F821
+    #     name=name,
+    #     cache_from=cache_from,
+    #     cache_to=cache_to,
+    #     **kwargs,
+    # )
 
     cache_from = kwargs.pop(
         "cache_from",
         {
             "type": "registry",
-            "ref": "ghcr.io/robotpajamas/dockerfiles:my-branch",
+            "ref": f"ghcr.io/robotpajamas/dockerfiles/build-cache:{name}",
         },
     )
     cache_to = kwargs.pop(
@@ -24,7 +24,7 @@ def docker_image_gha(name: str, **kwargs) -> None:
         {
             "type": "registry",
             "mode": "max",
-            "ref": "ghcr.io/robotpajamas/dockerfiles:my-branch",
+            "ref": f"ghcr.io/robotpajamas/dockerfiles/build-cache:{name}",
         },
     )
 
