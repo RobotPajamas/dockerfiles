9b8fb78b39680c6372e1de7ab7065bb975fc0dc8
SJ <suresh@robotpajamas.com>
2024-06-17 01:13:55 -0400

    Testing GHCR as a cache location (#35)
    Reviewing what happens on Merge

--- pants-plugins/macros.py
+++ pants-plugins/macros.py
@@ -2,11 +2,34 @@ def docker_image_gha(name: str, **kwargs) -> None:
     """
     A macro to create a Docker image that caches to Github Actions, by default.
     """
-    cache_from = kwargs.pop("cache_from", {"type": "gha", "scope": name})
-    cache_to = kwargs.pop("cache_to", {"type": "gha", "mode": "max", "scope": name})
+    # cache_from = kwargs.pop("cache_from", {"type": "gha", "scope": name})
+    # cache_to = kwargs.pop("cache_to", {"type": "gha", "mode": "max", "scope": name})
+
+    # docker_image(  # noqa: F821
+    #     name=name,
+    #     cache_from=cache_from,
+    #     cache_to=cache_to,
+    #     **kwargs,
+    # )
+
+    cache_from = kwargs.pop(
+        "cache_from",
+        {
+            "type": "registry",
+            "ref": f"ghcr.io/robotpajamas/dockerfiles/{name}/build-cache:main",
+        },
+    )
+    cache_to = kwargs.pop(
+        "cache_to",
+        {
+            "type": "registry",
+            "mode": "max",
+            "ref": f"ghcr.io/robotpajamas/dockerfiles/{name}/build-cache:main",
+        },
+    )
 
     docker_image(  # noqa: F821
-        name=name,
+        name=f"{name}-ghcr",
         cache_from=cache_from,
         cache_to=cache_to,
         **kwargs,
